41f838c102f401072bd7778707abd23ae055ab0e
Duncan Parkes <deparkes@gmail.com>
2020-01-11 15:41:46 +0000

    Add basic subprocess 1

--- example.py
+++ example.py
@@ -8,24 +8,36 @@ window.title("Main Window")
 menubar = tkinter.Menu(window)
 helpmenu = tkinter.Menu(menubar)
 
-def create_window():
-    new_window = tkinter.Toplevel(window)
-    new_window.title('About')
-    lbl = tkinter.Label(new_window, text="About")
+def create_about():
+    about_window = tkinter.Toplevel(window)
+    about_window.title('About')
+    lbl = tkinter.Label(about_window, text="About")
     lbl.config(anchor=tkinter.CENTER)
     lbl.pack(anchor='center')
-    btn = tkinter.Button(new_window, text="OK", command=new_window.destroy)
+    btn = tkinter.Button(about_window, text="OK", command=about_window.destroy)
     btn.pack(anchor='center')
 
+def create_subwindow1():
+    about_window = tkinter.Toplevel(window)
+    about_window.title('Subprocess 1')
+    lbl = tkinter.Label(about_window, text="Subprocess 1")
+    lbl.config(anchor=tkinter.CENTER)
+    lbl.pack(anchor='center')
+    btn = tkinter.Button(about_window, text="OK", command=about_window.destroy)
+    btn.pack(anchor='center')
 
 
 menubar.add_cascade(label="Help", menu=helpmenu)
 
-helpmenu.add_command(label="About", command=create_window)
+helpmenu.add_command(label="About", command=create_about)
 
 
 # display the menu
 window.config(menu=menubar)
+
+btn = tkinter.Button(window, text="Subprocess 1", command=create_subwindow1)
+btn.pack(anchor='center')
+
 
 window.mainloop()
 
