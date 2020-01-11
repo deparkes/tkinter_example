affd62223e7fa91fbad2527c0752abf95ddd61f6
Duncan Parkes <deparkes@gmail.com>
2020-01-11 15:44:56 +0000

    Add basic subwindow 2

--- example.py
+++ example.py
@@ -18,14 +18,22 @@ def create_about():
     btn.pack(anchor='center')
 
 def create_subwindow1():
-    about_window = tkinter.Toplevel(window)
-    about_window.title('Subprocess 1')
-    lbl = tkinter.Label(about_window, text="Subprocess 1")
+    sub_window = tkinter.Toplevel(window)
+    sub_window.title('Subprocess 1')
+    lbl = tkinter.Label(sub_window, text="Subprocess 1")
     lbl.config(anchor=tkinter.CENTER)
     lbl.pack(anchor='center')
-    btn = tkinter.Button(about_window, text="OK", command=about_window.destroy)
+    btn = tkinter.Button(sub_window, text="OK", command=sub_window.destroy)
     btn.pack(anchor='center')
 
+def create_subwindow2():
+    sub_window = tkinter.Toplevel(window)
+    sub_window.title('Subprocess 2')
+    lbl = tkinter.Label(sub_window, text="Subprocess 2")
+    lbl.config(anchor=tkinter.CENTER)
+    lbl.pack(anchor='center')
+    btn = tkinter.Button(sub_window, text="OK", command=sub_window.destroy)
+    btn.pack(anchor='center')
 
 menubar.add_cascade(label="Help", menu=helpmenu)
 
@@ -37,6 +45,9 @@ window.config(menu=menubar)
 
 btn = tkinter.Button(window, text="Subprocess 1", command=create_subwindow1)
 btn.pack(anchor='center')
+
+btn2 = tkinter.Button(window, text="Subprocess 2", command=create_subwindow2)
+btn2.pack(anchor='center')
 
 
 window.mainloop()
